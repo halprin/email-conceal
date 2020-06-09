f97f4af76dc3c54faa7449110a2bdde19c4d3098
halprin <halprin@users.noreply.github.com>
2020-06-08 19:53:51 -0600

    Save the description into DynamoDB with batch write

--- manager/gateways/configurationDynamo.go
+++ manager/gateways/configurationDynamo.go
@@ -8,6 +8,8 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 	"github.com/halprin/email-conceal/manager/context"
 	"github.com/halprin/email-conceal/manager/external/lib/errors"
+	"log"
+	"time"
 )
 
 var dynamoService = dynamodb.New(awsSession)
@@ -28,49 +30,25 @@ func AddConcealedEmailToActualEmailMapping(concealPrefix string, actualEmail str
 		return errors.Wrap(sessionErr, "Error with the AWS session")
 	}
 
-	tableName := aws.String(applicationContext.Gateways().GetEnvironmentValue("TABLE_NAME"))
-
-	//write the primary entity
-
+	//the primary entity
 	entity := ConcealEmailEntity{
 		Primary:   fmt.Sprintf("conceal-%s", concealPrefix),
 		Secondary: fmt.Sprintf("conceal-%s", concealPrefix),
 		Description: description,
 	}
-	dynamoEntity, err := dynamodbattribute.MarshalMap(entity)
-	if err != nil {
-		return errors.Wrap(err, "Failed to marshal struct into a DynamoDB item")
-	}
 
-	putItemInput := &dynamodb.PutItemInput{
-		TableName: tableName,
-		Item:      dynamoEntity,
-	}
-	_, err = dynamoService.PutItem(putItemInput)
-	if err != nil {
-		return errors.Wrap(err, "Failed to put entity in DynamoDB")
-	}
-
-	//write the mapping data for the conceal entity
+	//the mapping data for the conceal entity
 	mapping := ConcealEmailMapping{
 		Primary:   fmt.Sprintf("conceal-%s", concealPrefix),
 		Secondary: fmt.Sprintf("email-%s", actualEmail),
 	}
-	dynamoMapping, err := dynamodbattribute.MarshalMap(mapping)
-	if err != nil {
-		return errors.Wrap(err, "Failed to marshal struct into a DynamoDB item")
+
+	rollbackFromNewConceal := func(applicationContext context.ApplicationContext) {
+		_ = batchDeleteItemsWithRollback([]interface{}{entity, mapping}, nil, applicationContext)
 	}
 
-	putItemInput = &dynamodb.PutItemInput{
-		TableName: tableName,
-		Item:      dynamoMapping,
-	}
-	_, err = dynamoService.PutItem(putItemInput)
-	if err != nil {
-		return errors.Wrap(err, "Failed to put item in DynamoDB")
-	}
-
-	return nil
+	log.Println("Writing new conceal mapping to DynamoDB")
+	return batchWriteItemsWithRollback([]interface{}{entity, mapping}, rollbackFromNewConceal, applicationContext)
 }
 
 func DeleteConcealedEmailToActualEmailMapping(concealPrefix string, applicationContext context.ApplicationContext) error {
@@ -120,4 +98,102 @@ func getAllItemsForHashKey(hashKey string, tableName string) ([]map[string]*dyna
 	}
 
 	return queryOutput.Items, nil
+}
+
+func batchWriteItemsWithRollback(structsToWrite []interface{}, rollbackFunction func(context.ApplicationContext), applicationContext context.ApplicationContext) error {
+	log.Println("Batch writing items")
+	return batchInternal(structsToWrite, rollbackFunction, batchWrite, applicationContext)
+}
+
+func batchDeleteItemsWithRollback(structsToDelete []interface{}, rollbackFunction func(context.ApplicationContext), applicationContext context.ApplicationContext) error {
+	log.Println("Batch deleting items")
+	return batchInternal(structsToDelete, rollbackFunction, batchDelete, applicationContext)
+}
+
+const (
+	batchWrite = "batchWrite"
+	batchDelete = "batchDelete"
+)
+
+func batchInternal(structsToWrite []interface{}, rollbackFunction func(context.ApplicationContext), batchOperation string, applicationContext context.ApplicationContext) error {
+	//convert the structs to dynamo attribute maps
+	var dynamoItems []map[string]*dynamodb.AttributeValue
+
+	for _, structToWrite := range structsToWrite {
+		dynamoMapping, err := dynamodbattribute.MarshalMap(structToWrite)
+		if err != nil {
+			//return immediately without running the rollback function because we haven't even made a single DynamoDB call yet
+			return err
+		}
+
+		dynamoItems = append(dynamoItems, dynamoMapping)
+	}
+
+	//convert the dynamo attribute maps to write requests (structs needed by the WriteBatchItem API)
+	var writeRequests []*dynamodb.WriteRequest
+	var writeRequest *dynamodb.WriteRequest
+
+	for _, dynamoItem := range dynamoItems {
+		if batchOperation == batchWrite {
+			putRequest := &dynamodb.PutRequest{
+				Item:  dynamoItem,
+			}
+
+			writeRequest = &dynamodb.WriteRequest{
+				PutRequest: putRequest,
+			}
+		} else if batchOperation == batchDelete {
+			deleteRequest := &dynamodb.DeleteRequest{
+				Key:  dynamoItem,
+			}
+
+			writeRequest = &dynamodb.WriteRequest{
+				DeleteRequest: deleteRequest,
+			}
+		}
+
+		writeRequests = append(writeRequests, writeRequest)
+	}
+
+	//do last bit of construction for the BatchWriteItem API
+	tableName := applicationContext.Gateways().GetEnvironmentValue("TABLE_NAME")
+	requestItems := map[string][]*dynamodb.WriteRequest{
+		tableName: writeRequests,
+	}
+
+	//loop until all the remaining items have been written
+	millisecondsToWait := 20
+
+	for {
+		batchWriteItemInput := &dynamodb.BatchWriteItemInput{
+			RequestItems: requestItems,
+		}
+
+		batchWriteItemOutput, err := dynamoService.BatchWriteItem(batchWriteItemInput)
+		if err != nil {
+			//there was an error writing to DynamoDB
+			log.Println("Failed to put/delete items in DynamoDB")
+			if rollbackFunction != nil {
+				log.Println("Calling rollback function")
+				go rollbackFunction(applicationContext)
+			}
+			return errors.Wrap(err, "Failed to put/delete items in DynamoDB")
+		}
+
+		if len(batchWriteItemOutput.UnprocessedItems) > 0 {
+			//there are still items to write, reset requestItems for the next pass
+			log.Println("Unprocessed items remain, trying again with remaining items")
+			requestItems = batchWriteItemOutput.UnprocessedItems
+		} else {
+			//no more items to write, break out
+			log.Println("Done putting/deleting batch items to DynamoDB")
+			break
+		}
+
+		//do an exponential back-off
+		time.Sleep(time.Duration(millisecondsToWait) * time.Millisecond)
+		millisecondsToWait *= 2
+	}
+
+	return nil
 }
