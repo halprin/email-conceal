5ae60fc6bc2adc7b2d38b357f9692081a095e1be
halprin <halprin@users.noreply.github.com>
2020-12-19 17:29:12 -0700

    Clean-up the forwarder application contexts

--- src/external/cli/cliApplicationContext.go
+++ src/external/cli/cliApplicationContext.go
@@ -1,48 +1,15 @@
 package cli
 
 import (
-	"github.com/halprin/email-conceal/forwarder/gateways"
-	"github.com/halprin/email-conceal/forwarder/usecases"
 	"github.com/halprin/email-conceal/src/context"
 	forwardEmailController "github.com/halprin/email-conceal/src/controllers/forwardEmail"
-	"github.com/halprin/email-conceal/src/external/lib"
 	"github.com/halprin/email-conceal/src/gateways/awsSesSendEmail"
 	"github.com/halprin/email-conceal/src/gateways/dynamodb"
 	"github.com/halprin/email-conceal/src/gateways/localFileReader"
 	"github.com/halprin/email-conceal/src/gateways/osEnvironmentVariable"
 	forwardEmailUsecase "github.com/halprin/email-conceal/src/usecases/forwardEmail"
-	"os"
 )
 
-type CliApplicationContext struct{}
-
-func (cliAppContext *CliApplicationContext) ForwardEmailController(arguments map[string]interface{}) error {
-	return controllers.CliForwardEmail(arguments, cliAppContext)
-}
-
-func (cliAppContext *CliApplicationContext) ReadEmailGateway(url string) ([]byte, error) {
-	return gateways.FileReadEmailGateway(url, cliAppContext)
-}
-
-func (cliAppContext *CliApplicationContext) SendEmailGateway(email []byte, recipients []string) error {
-	return gateways.AwsSesSendEmailGateway(email, recipients, cliAppContext)
-}
-
-func (cliAppContext *CliApplicationContext) EnvironmentGateway(key string) string {
-	return gateways.OsEnvEnvironmentGateway(key, cliAppContext)
-}
-
-func (cliAppContext *CliApplicationContext) GetRealEmailForConcealPrefix(concealPrefix string) (string, error) {
-	return gateways.GetRealEmailForConcealPrefix(concealPrefix, cliAppContext)
-}
-
-func (cliAppContext *CliApplicationContext) ForwardEmailUsecase(url string) error {
-	return usecases.ForwardEmailUsecase(url, cliAppContext)
-}
-
-func (cliAppContext *CliApplicationContext) Exit(returnCode int) {
-	os.Exit(returnCode)
-}
 
 func init() {
 	var applicationContext = context.ApplicationContext{}
--- src/external/sqsQueue/sqsQueueApplicationContext.go
+++ src/external/sqsQueue/sqsQueueApplicationContext.go
@@ -1,49 +1,14 @@
 package sqsQueue
 
 import (
-	"github.com/halprin/email-conceal/forwarder/gateways"
-	"github.com/halprin/email-conceal/forwarder/usecases"
 	"github.com/halprin/email-conceal/src/context"
 	forwardEmailController "github.com/halprin/email-conceal/src/controllers/forwardEmail"
-	"github.com/halprin/email-conceal/src/external/lib"
 	"github.com/halprin/email-conceal/src/gateways/awsSesSendEmail"
 	"github.com/halprin/email-conceal/src/gateways/dynamodb"
-	"github.com/halprin/email-conceal/src/gateways/localFileReader"
 	"github.com/halprin/email-conceal/src/gateways/osEnvironmentVariable"
 	"github.com/halprin/email-conceal/src/gateways/s3FileReader"
 	forwardEmailUsecase "github.com/halprin/email-conceal/src/usecases/forwardEmail"
-	"os"
 )
-
-type SqsQueueApplicationContext struct{}
-
-func (appContext *SqsQueueApplicationContext) ForwardEmailController(arguments map[string]interface{}) error {
-	return controllers.SqsQueueForwardEmail(arguments, appContext)
-}
-
-func (appContext *SqsQueueApplicationContext) ReadEmailGateway(url string) ([]byte, error) {
-	return gateways.S3ReadEmailGateway(url, appContext)
-}
-
-func (appContext *SqsQueueApplicationContext) SendEmailGateway(email []byte, recipients []string) error {
-	return gateways.AwsSesSendEmailGateway(email, recipients, appContext)
-}
-
-func (appContext *SqsQueueApplicationContext) EnvironmentGateway(key string) string {
-	return gateways.OsEnvEnvironmentGateway(key, appContext)
-}
-
-func (appContext *SqsQueueApplicationContext) GetRealEmailForConcealPrefix(concealPrefix string) (string, error) {
-	return gateways.GetRealEmailForConcealPrefix(concealPrefix, appContext)
-}
-
-func (appContext *SqsQueueApplicationContext) ForwardEmailUsecase(url string) error {
-	return usecases.ForwardEmailUsecase(url, appContext)
-}
-
-func (appContext *SqsQueueApplicationContext) Exit(returnCode int) {
-	os.Exit(returnCode)
-}
 
 
 func init() {
