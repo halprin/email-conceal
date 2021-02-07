4f2b828d7690a61cb685947d67fe70556bd7dc9a
halprin <halprin@users.noreply.github.com>
2021-02-06 19:53:15 -0700

    Actual e-mail usecase valides e-mail address

--- src/usecases/actualEmail/actualEmail.go
+++ src/usecases/actualEmail/actualEmail.go
@@ -1,5 +1,6 @@
 package actualEmail
 
+import "github.com/halprin/email-conceal/src/entities"
 
 type ActualEmailUsecase interface {
 	Add(actualEmail string) error
@@ -8,5 +9,14 @@ type ActualEmailUsecase interface {
 type ActualEmailUsecaseImpl struct {}
 
 func (receiver ActualEmailUsecaseImpl) Add(actualEmail string) error {
+	err := entities.ValidateEmail(actualEmail)
+	if err != nil {
+		return err
+	}
+
+	//TODO: generate the secret
+	//TODO: write actual e-mail with secret to database
+	//TODO: send registration e-mail
+
 	return nil
 }
