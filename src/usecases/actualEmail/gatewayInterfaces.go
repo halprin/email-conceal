2d21359754f31b544cbe75b511a888fb597faf5b
halprin <halprin@users.noreply.github.com>
2022-01-09 14:49:10 -0700

    Add TODO reminders

--- src/usecases/actualEmail/gatewayInterfaces.go
+++ src/usecases/actualEmail/gatewayInterfaces.go
@@ -1,9 +1,11 @@
 package actualEmail
 
+//TODO: need to implement
 type SendRegistrationEmailGateway interface {
 	SendEmail(email []byte, recipient string) error
 }
 
+//TODO: need to implement
 type ActualEmailConfigurationGateway interface {
 	AddUnprovedActualEmail(actualEmail string, ownershipSecret string) error
 }
