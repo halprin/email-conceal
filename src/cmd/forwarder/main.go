ed3dbcdefbab4f6b0ec87168cc9ebfb4c4463bf5
halprin <halprin@users.noreply.github.com>
2022-01-09 18:18:47 -0700

    Run forwarder locally programatically

--- src/cmd/forwarder/main.go
+++ src/cmd/forwarder/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"github.com/halprin/email-conceal/src/external/localFileWatch"
 	"github.com/halprin/email-conceal/src/external/sqsQueue"
+	"os"
 )
 
 func main() {
+	environment := os.Getenv("ENVIRONMENT")
+	if environment == "local" {
+		localFileWatch.Init()
+		localFileWatch.LocalFileWatcher()
+		return
+	}
+
+	sqsQueue.Init()
 	sqsQueue.SqsQueueListener()
 	//uncomment below to test locally
 	//localFileWatch.LocalFileWatcher()
