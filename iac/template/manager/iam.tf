d4e4dcf282352e803227ca37b4d60547fc0ae756
halprin <halprin@users.noreply.github.com>
2022-01-12 22:58:44 -0700

    Create the Manager Lambda

---
+++ iac/template/manager/iam.tf
@@ -0,0 +1,68 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "execution_role" {
+  name = "email-conceal-manager-${var.environment}"
+
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_policy" "permissions_for_manager" {
+  name   = "email-conceal-manager-${var.environment}"
+  policy = data.aws_iam_policy_document.permissions.json
+}
+
+data "aws_iam_policy_document" "permissions" {
+  statement {
+    sid    = "DecryptDynamoDb"
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+      "kms:DescribeKey",
+    ]
+    resources = [var.application_key_arn]
+  }
+
+  statement {
+    sid       = "SendEmail"
+    effect    = "Allow"
+    actions   = ["ses:SendRawEmail"]
+    resources = ["arn:aws:ses:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:identity/${var.domain}"]
+  }
+
+  statement {
+    sid    = "ReadConfigurationFromDynamo"
+    effect = "Allow"
+    actions = [
+      "dynamodb:Query",
+    ]
+    resources = ["arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${var.configuration_database_name}"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "attach_permission_to_role" {
+  role       = aws_iam_role.execution_role.name
+  policy_arn = aws_iam_policy.permissions_for_manager.arn
+}
+
+data "aws_iam_policy" "lambda_basic_execution" {
+  name = "AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "attach_log_permission_to_role" {
+  role       = aws_iam_role.execution_role.name
+  policy_arn = data.aws_iam_policy.lambda_basic_execution.arn
+}
+
---
+++ iac/template/manager/lambda.tf
@@ -0,0 +1,32 @@
+resource "aws_lambda_function" "api_lambda" {
+  function_name = "email-conceal-manager-${var.environment}"
+
+  filename = data.archive_file.lambda_zip_archive.output_path
+  source_code_hash = data.archive_file.lambda_zip_archive.output_base64sha256
+  handler = "main.LambdaHandler"
+  timeout = 10
+  memory_size = 128
+  runtime = "go1.x"
+
+  environment {
+    variables = {
+      DOMAIN                 = var.domain
+      TABLE_NAME             = var.configuration_database_name
+      ENVIRONMENT            = var.environment
+      FORWARDER_EMAIL_PREFIX = var.forward_email_prefix
+    }
+  }
+
+  role = aws_iam_role.execution_role.arn
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+data "archive_file" "lambda_zip_archive" {
+  type             = "zip"
+  source_file      = "${path.module}/../../../src/manager"
+#  output_file_mode = "0666"
+  output_path      = "${path.module}/manager_lambda.zip"
+}
