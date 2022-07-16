f0ce5e5394377e40e8cb80c6718dedc5b17c95d7
halprin <halprin@users.noreply.github.com>
2022-07-16 14:53:22 -0600

    Update navigation

--- frontend/src/navigation/Navigation.jsx
+++ frontend/src/navigation/Navigation.jsx
@@ -1,17 +1,30 @@
 const Navigation = () => {
-    return <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-        <a className='navbar-brand active'>E-mail Conceal</a>
-        <button className='navbar-toggler' type='button' data-bs-toggle='collapse'
-                data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false'
-                aria-label='Toggle navigation'>
-            <span className='navbar-toggler-icon'></span>
-        </button>
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-            <div className='navbar-nav'>
-                <a className='nav-link' href='./login' >Login</a>
+    return (
+        <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+            <a className='navbar-brand active'>E-mail Conceal</a>
+            <button className='navbar-toggler' type='button' data-bs-toggle='collapse'
+                    data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false'
+                    aria-label='Toggle navigation'>
+                <span className='navbar-toggler-icon'></span>
+            </button>
+            <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+                <ul className='navbar-nav'>
+                    <li className='nav-item'>
+                        <a className='nav-link' href='./create-conceal-email'>Create Concealed E-mail</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a className='nav-link' href='./update-conceal-email'>Update Concealed E-mail</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a className='nav-link' href='./delete-conceal-email'>Delete Concealed E-mail</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a className='nav-link' href='./login'>Login</a>
+                    </li>
+                </ul>
             </div>
-        </div>
-    </nav>;
+        </nav>
+    );
 }
 
 export default Navigation;
